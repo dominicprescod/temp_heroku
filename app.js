c49ccafc80b07fe3f66db7dd6b498f516ff11dd0
dominicprescod <dominicprescod@gmail.com>
2017-01-17 12:58:35 -0500

    postgres

--- app.js
+++ app.js
@@ -1,17 +1,32 @@
 var express     = require('express'),
-    app         = exppress(),
+    app         = express(),
     fs          = require('fs'),
-    bodyParser  = require('body-parser')
-    port        = process.ENV.port || 3000;
+    bodyParser  = require('body-parser'),
+    port        = process.env.PORT || 3000,
+    pg                = require('pg'),
+    sql               = require('knex')({
+                          client: 'pg',
+                          connection: 'postgres://localhost:5432/dprescod',
+                          searchPath: 'dprescod,public'
+                        });
 
+
+
+    pg.defaults.ssl = (process.env.DATABASE_URL) ? true : undefined;
+        pg.connect(process.env.DATABASE_URL || 'postgres://localhost:5432/dprescod', function(er, client) {
+          if(er) throw er;
 
 
     app.use(bodyParser.json({limit: '50mb'}));
 
     app.post('/new', function(req, res){
-      fs.writeFileSync('./file.json', JSON.stringify(req.body), 'utf-8')
-      res.send(req.body)
-    })
+      client.query(sql.insert(req.body).into("stuff").toString())
+          .on('end', function(result){
+            res.send(req.body)
+          });
+    });
+
     app.listen(port, function(){
-      console.log("I'm on")
-    })
+          console.log("I'm on!");
+        });
+      });
