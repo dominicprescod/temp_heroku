4cc2ec8b3a3299243d1d5567d29c5019411ecee5
dominicprescod <dominicprescod@gmail.com>
2017-01-17 12:25:41 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+var express     = require('express'),
+    app         = exppress(),
+    fs          = require('fs'),
+    bodyParser  = require('body-parser')
+    port        = process.ENV.port || 3000;
+
+
+
+    app.use(bodyParser.json({limit: '50mb'}));
+
+    app.post('/new', function(req, res){
+      fs.writeFileSync('./file.json', JSON.stringify(req.body), 'utf-8')
+    })
+    app.listen(port, function(){
+      console.log("I'm on")
+    })
